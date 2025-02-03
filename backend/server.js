020da93a77c822630bc003d2c93a6476dbb9e010
aniket0806 <97946316+aniket0806@users.noreply.github.com>
2025-02-03 23:31:42 +0530

    last

--- backend/server.js
+++ backend/server.js
@@ -20,12 +20,13 @@ const app = express();
 //   })
 // );
 
-
-app.use(cors({
-  origin: 'https://task-management-system-snowy-kappa.vercel.app', // Remove '/login'
-  credentials: true
-}));
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://task-management-system-snowy-kappa.vercel.app"); // No '/login'
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
